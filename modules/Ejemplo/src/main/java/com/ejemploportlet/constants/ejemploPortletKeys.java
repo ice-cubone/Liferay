49b94093683f7f98cbfbe652ec062a183e7b1aa6
ice-cubone <lrodriguez@grupodot.com>
2017-12-07 09:09:42 -0500

    subir un archivo

---
+++ modules/Ejemplo/src/main/java/com/ejemploportlet/constants/ejemploPortletKeys.java
@@ -0,0 +1,10 @@
+package com.ejemploportlet.constants;
+
+/**
+ * @author grupodot
+ */
+public class ejemploPortletKeys {
+
+	public static final String ejemplo = "ejemplo";
+
+}
---
+++ modules/Ejemplo/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/init.jsp" %>
+
+<center>
+<font  color="#DF7401" size="20">
+Bienvenido  a nuestro portal 
+</font>
+</center>
